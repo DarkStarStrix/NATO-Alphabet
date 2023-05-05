543ac50c15edaaeb8d87d108eeb8bd2d9ed5e0d9
Allan Murimi Wandia <108637439+DarkStarStrix@users.noreply.github.com>
2023-05-05 10:27:57 -0400

    Add files via upload

--- NATO-alphabet-start/NATO-alphabet-start/main.py
+++ NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -1,27 +1,24 @@
-student_dict = {
-    "student": ["Angela", "James", "Lily"], 
-    "score": [56, 76, 98]
-}
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
+# NATO alphabet translator
 
 import pandas
-student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# Read data from csv file
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# Create a dictionary from the data
+nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(nato_dict)
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        nato_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(nato_list)
 
+
+generate_phonetic()
